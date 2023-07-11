82f1635c3e169335452f226d9b519229fb87ba41
Amit Vishwakarma <amit200698@gmail.com>
2023-07-11 23:22:03 +0530

    here created model admin page add employee sign in and signup part

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../model/users");
+
 module.exports.signIn = async function(req, res){
     try {
         return res.render('user_sign_in', {
@@ -17,6 +19,29 @@ module.exports.signUp = async function(req, res){
             title: "Employee Review System | Sign Up"
         })
     } catch (error) {
+        console.log("Error", error);
+        return;
+    }
+}
+
+module.exports.create = async function(req, res){
+    try {
+        if(req.body.password != req.body.c_password){
+            return res.redirect('back');
+        }
+        let createUser = await User.findOne({email: req.body.email});
+        if(!createUser){
+            await User.create(req.body);
+        }else{
+            return res.redirect('back');
+        }
+        return res.redirect('/user/sign-in');
+    } catch (error) {
+        console.log("Error", error)
         
     }
+}
+
+module.exports.createSession = async function(req, res){
+    return res.redirect('back')
 }
--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,7 @@ const userController = require('../controller/users_controller');
 
 router.get('/sign-in', userController.signIn);
 router.get('/sign-up', userController.signUp);
+router.post('/create', userController.create);
+router.post('/create-session', userController.createSession);
 
 module.exports = router;
