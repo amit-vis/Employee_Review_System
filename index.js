a2a5c21142519ccb7557af1ce774c49cd03d8532
Amit Vishwakarma <amit200698@gmail.com>
2023-08-02 14:26:48 +0530

    we have made some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 5000;
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
 const db = require('./config/mongoose');
